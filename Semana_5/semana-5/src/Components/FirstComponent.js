a324f50f80338ec489f069ff7df2315c6cc8e986
BernardoSesiSenai <bernardo_ramires@estudante.sesisenai.org.br>
2023-02-24 01:36:39 -0300

    Criado o primeiro componente igual o exemplo.

---
+++ Semana_5/semana-5/src/Components/FirstComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FirstComponent = () => {
+  return (
+    <div>Exemplo de component:
+        <h1>This is my first component.</h1>
+    </div>
+  )
+}
+
+export default FirstComponent
